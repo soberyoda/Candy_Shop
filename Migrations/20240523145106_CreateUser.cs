82c8f1652734c74f7d795666fcc2842c92b531cd
Spookyless <spookyless.info@gmail.com>
2024-05-23 20:48:32 +0200

    Add migration

---
+++ Migrations/20240523145106_CreateUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Candy_Shop.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "username",
+                keyValue: "admin",
+                column: "type",
+                value: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "username",
+                keyValue: "admin",
+                column: "type",
+                value: 1);
+        }
+    }
+}
