b56bc4b55a8ce53abf16304a7b22318cdac00521
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-05-24 15:17:49 +0200

    user migration

--- Migrations/20240523145106_CreateUser.cs
+++ Migrations/20240523145106_CreateUser.cs
@@ -10,23 +10,30 @@ namespace Candy_Shop.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.UpdateData(
-                table: "Users",
-                keyColumn: "username",
-                keyValue: "admin",
-                column: "type",
-                value: 2);
+            migrationBuilder.CreateTable(
+              name: "Users",
+              columns: table => new
+              {
+                username = table.Column<string>(type: "TEXT", nullable: false),
+                password = table.Column<string>(type: "TEXT", nullable: false),
+                apiToken = table.Column<string>(type: "TEXT", nullable: false),
+                type = table.Column<int>(type: "INTEGER", nullable: false)
+              },
+              constraints: table =>
+              {
+                table.PrimaryKey("PK_Users", x => x.username);
+              });
+            migrationBuilder.InsertData(
+              table: "Users",
+              columns: new[] { "username", "apiToken", "password", "type" },
+              values: new object[] { "admin", "aae1e103-bca5-9fa1-ba8c-42058b4abf28", "21232F297A57A5A743894A0E4A801FC3", 2 });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.UpdateData(
-                table: "Users",
-                keyColumn: "username",
-                keyValue: "admin",
-                column: "type",
-                value: 1);
+          migrationBuilder.DropTable(
+            name: "Users");
         }
     }
 }
